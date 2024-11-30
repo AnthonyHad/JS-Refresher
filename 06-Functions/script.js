74fdd37f5ee3c91153e96eb8f1e35bcea8579abe
Anthony Hadwan <anthony.hadwan@hec.edu>
2024-11-30 19:41:33 +0100

    closures

--- 06-Functions/script.js
+++ 06-Functions/script.js
@@ -82,3 +82,73 @@ greet("Hello")("Anthony");
 const greetArr = (greeting) => (name) => {
   console.log(`${greeting} ${name}`);
 };
+
+// Call and Apply
+
+const middleEast = {
+  airline: "Middle East Airline",
+  iataCode: "MEA",
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+  },
+};
+
+const euroWings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+
+const book = middleEast.book;
+
+// book(23, "Anthony") // This will point to undefined as this is a regualar function call.
+
+book.call(euroWings, 23, "Anthony"); // Sets the this keyword to eurowings
+
+// Apply method takes in an array of data
+
+const flightData = [583, "George Cooper"];
+book.apply(euroWings, flightData);
+
+// or and which is better
+
+book.call(euroWings, ...flightData);
+
+// Bind Method
+// It returns a new function
+
+const bookEW = book.bind(euroWings);
+bookEW(23, "Steven Williams");
+
+// IIFE
+
+(function () {
+  console.log("This will never run again");
+})();
+
+(() => console.log("This will never run again"))();
+
+// Closures
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(passengerCount);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+// Calling booker again despite the fact that the execution context of securebooking is gone is possible because of closures
+// A function has access to the variable env of the execution context where it was created
+//  CLosure has priority over scope chain
+// Remembers variables even though function has returned
+
+console.dir(booker); // Can check closures here
